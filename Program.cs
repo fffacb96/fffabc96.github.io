541522851126e16993e99ed559ba412383757fa9
6d486f49 <76097bcc@gmail.com>
2025-10-25 15:07:55 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Lydia;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using Blazor.Analytics;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddGoogleAnalytics("G-8RXPFTJQRP");
+
+await builder.Build().RunAsync();
