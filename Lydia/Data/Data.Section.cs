170da7c18da23407e08cacb65669b530bf9402f6
6d486f49 <76097bcc@gmail.com>
2025-10-26 15:01:30 -0400

    More converted data

--- Lydia/Data/Data.Section.cs
+++ Lydia/Data/Data.Section.cs
@@ -21,6 +21,7 @@ public class Painting : Section
     public required string Descriptions { get; set; }
 }
 
+
 public static partial class Data
 {
     public static Painting[] Paintings =
@@ -139,7 +140,250 @@ public static partial class Data
                 Inspired by natures beauty daffodils and pussy willows emerge in spring for Easter blessings.
                 """
         },
-        
+        new()
+        {
+            Order = 16,
+            File = "16.webp",
+            Available = true,
+            Name = "Les tournesols / The sunflowers",
+            Size = "Original size: 11\"",
+            Descriptions = 
+                """
+                Carved on wood in 1955. Frame of a plate. Painted with oil colours and lightly varnished.
+
+                She always like sunflowers and would plant them in her garden.
+                """
+        },
+        new()
+        {
+            Order = 17,
+            File = "17.webp",
+            Available = true,
+            Name = "Assiette décorative / Decorative plate",
+            Size = "Original size: 10\"",
+            Descriptions = 
+                """
+                Carved on wood in 1955. Frame of a plate is carved using carving knives followed by pyrography, delicately burning wood. Painted with oil colours and lightly varnished.
+                """
+        },
+        new()
+        {
+            Order = 18,
+            File = "18.webp",
+            Available = true,
+            Name = "Assiette Lithuanienne / Lithuanien Plate",
+            Size = "Original size: 12\"",
+            Descriptions = 
+                """
+                Carved on wood in 1955. Frame of a plate is sanded down to give a wavy surface, followed by pyrography, delicately burning wood. Painted with oil colours and lightly varnished.
+
+                The colours she used are symbolic to the Lithuanian flag, yellow, green, and red.
+                """
+        },
+        new()
+        {
+            Order = 19,
+            File = "19.webp",
+            Available = true,
+            Name = "Trois pensée / Three pansies",
+            Size = "Original size: 11\"",
+            Descriptions = 
+                """
+                Carved on wood in 1955. Frame of a plate is sanded down to give a wavy surface, followed by carving the details. Painted with oil colours and lightly varnished.
+
+                She liked her flowers and would grow them in her garden. She was very thoughtful.
+                """
+        },
+        new()
+        {
+            Order = 20,
+            File = "20.webp",
+            Available = true,
+            Name = "Nocturne / Night",
+            Size = "Original size: 16 x 20",
+            Descriptions = 
+                """
+                Copper year 1967. Push-back method used on copper. The back of the painting is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in. Varnished to preserve a shine.
+
+                Three laddies sitting on a rock, basking in the moonlight ocean to cool from the hot summer nights.
+                """
+        },
+        new()
+        {
+            Order = 21,
+            File = "21.webp",
+            Available = true,
+            Name = "Jeune fille aux tresses / Young girl with braids",
+            Size = "Original size: 11 x 18",
+            Descriptions = 
+                """
+                Portrait made back in 1964. Push-back method used on copper. The back of the painting is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in.
+
+                I am pretty sure it was a self portrait when she was younger as she did wear braids. Then again so did her girls.
+                """
+        },
+        new()
+        {
+            Order = 22,
+            File = "22.webp",
+            Available = true,
+            Name = "Koutchoum",
+            Size = "Original size: 15 x 19",
+            Descriptions = 
+                """
+                Painting was done in 1964.
+
+                Push-back method used on copper. The back of the painting is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in. Lots of detail was put in.
+
+                Her beloved German shepherd when he passed away, she made a portrait of him, to never be forgotten.
+                """
+        },
+        new()
+        {
+            Order = 23,
+            File = "23.webp",
+            Available = true,
+            Name = "Annonciation / Annunciation",
+            Size = "Original size: 15 x 19",
+            Descriptions = 
+                """
+                Painting was done in 1967.
+
+                Push-back method used on copper. The back of the painting is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in.
+
+                The announcement of the Incarnation by the angel Gabriel to virgin Mary.
+                """
+        },
+        new()
+        {
+            Order = 24,
+            File = "24.webp",
+            Available = true,
+            Name = "Conte d'andersen / Andersen's Tale",
+            Size = "Original size: 18 x 18",
+            Descriptions = 
+                """
+                Painting was done in 1961.
+
+                Push-back method used on tin. The back is filed with wax. Which was heated then poured onto the tin. Preventing the painting to cave in.
+
+                It's a story of a princess who rescues her 11 brothers from a spell cast by an evil queen. In this case she shows 5 swans representing her 5 girls.
+                """
+        },
+        new()
+        {
+            Order = 25,
+            File = "25.webp",
+            Available = true,
+            Name = "Danseuses / Dancers",
+            Size = "Original size: 11 x 15",
+            Descriptions = 
+                """
+                Painting was done in 1961.
+
+                Push-back method used on copper. The back of the painting is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in.
+
+                Three dancing gypsies. Gypsy dance is, dynamic and entertaining. Russian folk dance dated back around the 19th century.
+                """
+        },
+        new()
+        {
+            Order = 26,
+            File = "26.webp",
+            Available = true,
+            Name = "Fontaine de jouvence / Fountain of Youth",
+            Size = "Original size: 11 x 15",
+            Descriptions = 
+                """
+                Year 1961. Push-back method used on copper. The back is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in. Dark areas are blackened with black oxide, then varnished to preserve a shine on some areas.
+
+                Mythical fountainallegedly restores the youth to anyone who baths in it.
+                """
+        },
+        new()
+        {
+            Order = 27,
+            File = "27.webp",
+            Available = true,
+            Name = "Quadrigue / Quadriga",
+            Size = "Original size: 11 x 11",
+            Descriptions = 
+                """
+                Year 1961. Push-back method used on copper. The back is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in. Dark areas are blackened with black oxide, then varnished to preserve a shine on some areas.
+
+                Historically famous for the Brandenburg gate quadriga, 4 majestic horses.
+                """
+        },
+        new()
+        {
+            Order = 28,
+            File = "28.webp",
+            Available = true,
+            Name = "Feuilles d'Erable / Maple leaves",
+            Size = "Original size: 11 x 12",
+            Descriptions = 
+                """
+                Year 1960.  Push-back method used on copper. The back of the painting is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in.  Dark areas are blackened with black oxide, then varnished to preserve a shine on surfaced areas.
+
+                Symbolic to the beauty of maple leaves.
+                """
+        },
+        new()
+        {
+            Order = 29,
+            File = "29.webp",
+            Available = true,
+            Name = "Princesse Javakha / Princess Javakha",
+            Size = "Original size: 11 x 14",
+            Descriptions = 
+                """
+                Year 1960.  Push-back method used on copper. The back is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in.  Dark areas are blackened with black oxide, then varnished to preserve a shine on surfaced areas.
+
+                Folk tale. Princess Javakha escaping her rivals on a horse.
+                """
+        },
+        new()
+        {
+            Order = 30,
+            File = "30.webp",
+            Available = true,
+            Name = "Loreley / Loreley",
+            Size = "Original size: 18 x 15",
+            Descriptions = 
+                """
+                Year 1963.  Push-back method used on copper. The back is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in.  Dark areas are blackened with black oxide, then varnished to preserve a shine on surfaced areas.
+
+                The beautiful siren/mermaid whose songs enchanted passing sailors.
+                """
+        },
+        new()
+        {
+            Order = 31,
+            File = "31.webp",
+            Available = true,
+            Name = "Colibris / Hummingbirds",
+            Size = "Original size: 15 x 19",
+            Descriptions = 
+                """
+                Year 1961. Push-back method used on copper. The back is filled with wax. Which was heated then poured onto the copper. Preventing the painting to cave in.  Dark areas are blackened with black oxide, then varnished to preserve a shine on surfaced areas.
+
+                Inspiring sign of hope & good luck. The spirit of a loved one is nearby.
+                """
+        },
+        new()
+        {
+            Order = 32,
+            File = "32.webp",
+            Available = true,
+            Name = "Notre-Dame du Perpétuel Secours / Our Lady of Perpetual Help",
+            Size = "Original size: 21 x 27",
+            Descriptions = 
+                """
+                Copper year 1962 - Tin 1971. Push-back method used on both. The back is filled with wax. Which was heated then poured onto the copper and tin. Preventing the painting to cave in.  Dark areas are blackened with black oxide, then varnished to preserve a shine on surfaced areas.
+
+                Mother of God holding the Divine Child
+                """
+        },
 
     ];
 }
