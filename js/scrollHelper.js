7a8d3969b64cb274b7f9a68c717c8f5363254cb7
fffacb96 <fffacb96@users.noreply.github.com>
2026-05-18 17:56:50 +0000

    deploy: a510cb322489f43b5ecdd842c10683a260ee2c47

---
+++ js/scrollHelper.js
@@ -0,0 +1,8 @@
+window.scrollHelper = {
+    scrollTo: function (key) {
+        document.getElementById(key).scrollIntoView({behavior: 'smooth'})
+    },
+    goTo: function (key) {
+        document.getElementById(key).scrollIntoView({behavior: 'instant'})
+    },
+};
